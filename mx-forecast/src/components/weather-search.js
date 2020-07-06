c64d439ba8c855e973a40d7ad1e6cd7bf55ff3b8
Luke Vasilion <LVasilion@cleaverbrooks.com>
2020-07-06 15:43:15 -0400

    function added for track to city convert

--- mx-forecast/src/components/weather-search.js
+++ mx-forecast/src/components/weather-search.js
@@ -9,9 +9,28 @@ const api ={
 function WeatherSearch() {
     const [city, setCity] = useState('');
     const [weather, setWeather] = useState({});
-  
+    let input;
+    
+
+  function cityCheck(city){
+         if(city.toUpperCase() === "gpf".toUpperCase()){
+            setCity('cairo') 
+            return city;           
+        }
+        else if(city.toUpperCase() === "georgia practice facility".toUpperCase()){
+            setCity('cairo') 
+            return city;          
+        }
+        else{
+            setCity(city)
+             return city;
+        }
+       
+        
+    }
+
     const search = evt => {
-      if (evt.key === "Enter") {
+      if (evt.key === "Enter") {        
         fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)  
           .then(res => res.json())
           .then(result => {
@@ -33,6 +52,8 @@ function WeatherSearch() {
   
       return `${day} ${date} ${month} ${year}`
     }
+
+    
   
     return (
       <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 45) ? 'app warm' : 'app') : 'app'}>
@@ -42,16 +63,16 @@ function WeatherSearch() {
             <input 
               type="text"
               className="search-bar"
-              placeholder="Search..."
-              onChange={e => setCity(e.target.value)}
-              value={city}
+              placeholder="Enter city name"
+              onChange={e => cityCheck(e.target.value)}
+              value={input}
               onKeyPress={search}
             />
           </div>
           {(typeof weather.main != "undefined") ? (
           <div>
             <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.state}</div>
+              <div className="location">{weather.name}</div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
